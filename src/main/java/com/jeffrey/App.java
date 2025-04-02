e4748b4cc41b5af9655f1f4587c559d795456e52
Jeffrey <894860131@qq.com>
2025-04-02 15:09:09 +0800

    1-2 修改启动文案

--- src/main/java/com/jeffrey/App.java
+++ src/main/java/com/jeffrey/App.java
@@ -1,9 +1,12 @@
 package com.jeffrey;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * Hello world!
@@ -14,9 +17,12 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.jeffrey.lab.mapper")
 public class App 
 {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class,args);
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
