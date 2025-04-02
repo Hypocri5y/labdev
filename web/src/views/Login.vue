4f4de33c83d484062662b044669315a04b780893
Jeffrey <894860131@qq.com>
2025-04-02 22:03:29 +0800

    2-6 修改axios的baseURL读配置文件

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -3,8 +3,8 @@
     <a-layout-content style="display: flex; justify-content: center; align-items: center;">
       <a-card title="登录" style="width: 400px">
         <a-form :model="formState" @finish="handleLogin">
-          <a-form-item label="用户名" name="username" :rules="[{ required: true, message: '请输入用户名!' }]">
-            <a-input v-model:value="formState.username" placeholder="请输入用户名" />
+          <a-form-item label="用户名" name="loginName" :rules="[{ required: true, message: '请输入用户名!' }]">
+            <a-input v-model:value="formState.loginName" placeholder="请输入用户名" />
           </a-form-item>
           <a-form-item label="密码" name="password" :rules="[{ required: true, message: '请输入密码!' }]">
             <a-input-password v-model:value="formState.password" placeholder="请输入密码" />
@@ -26,13 +26,13 @@ import axios from "axios";
 export default {
   setup() {
     const formState = reactive({
-      username: '',
+      loginName: '',
       password: '',
     });
 
     const handleLogin = () => {
-      axios.post('http/user/login', {
-        username: formState.username,
+      axios.post('/user/login', {
+        loginName: formState.loginName,
         password: formState.password,
       })
           .then((resp) => {
@@ -45,11 +45,6 @@ export default {
               message.error(resp.data.msg || '登录失败，请重试！');
             }
           })
-          .catch((err) => {
-            console.error(err);
-            // 提供用户友好的错误提示
-            message.error('请求失败，请检查网络连接或稍后再试！');
-          });
     };
 
     // 返回响应式数据和方法，供模板使用
