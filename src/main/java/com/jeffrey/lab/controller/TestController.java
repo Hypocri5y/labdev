2c6918282334c115b827026be35c870d06b46745
Jeffrey <894860131@qq.com>
2025-04-02 15:17:20 +0800

    1-4 开发测试接口，验证各配置项

---
+++ src/main/java/com/jeffrey/lab/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.jeffrey.lab.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: lab
+ * @author: Jeffrey
+ * @create: 2025-04-02 15:11
+ * @description:
+ **/
+@RestController()
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "helloword!";
+    }
+}
