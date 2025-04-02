b887f86aa06798a31f4d81f8a01afd16a6e3b618
Jeffrey <894860131@qq.com>
2025-04-02 16:57:39 +0800

    1-9 集成ant design vue 加入示例按钮

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <a-button type="primary">Primary Button</a-button>  </div>
 </template>
 
 <script lang="ts">
